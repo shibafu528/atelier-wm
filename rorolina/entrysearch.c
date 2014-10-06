d2e3560d078c5c83671036c99465c7464d8aae54
shibafu <shibafu528@gmail.com>
2014-10-06 11:42:30 +0900

    デスクトップエントリパーサ書き始めた

--- rorolina/entrysearch.c
+++ rorolina/entrysearch.c
@@ -1,10 +1,31 @@
-#include <gtk/gtk.h>
+#include <glib.h>
+#include <glib/gstdio.h>
+#include <string.h>
 
-static typedef struct {
+typedef struct {
     gchar *name;
     gchar *exec;
     gchar *icon;
 } DesktopEntry;
+
+static DesktopEntry* read_desktop_entry(const gchar* path) {
+    const char *IDENTIFY = "[Desktop Entry]\n";
+    const int LINE_LENGTH = 1024;
+    FILE *fp;
+    char line[LINE_LENGTH];
+    
+    fp = g_fopen(path, "r");
+
+    //ヘッダチェック
+    if (fgets(line, LINE_LENGTH, fp) != NULL && strcmp(path, line) == 0) {
+        //TODO: ここから先でname, exec, iconを読み込んでいく
+        g_fprintf(stderr, "Identify OK: %s\n", path);
+    } else {
+        g_fprintf(stderr, "Identify Error: %s\n", path);
+    }
+
+    fclose(fp);
+}
 
 GList* get_application_list() {
     return NULL;
