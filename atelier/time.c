dab4421996f88e514ba96d1b5dc76a66d236672a
shibafu <shibafu528@gmail.com>
2015-01-14 09:42:01 +0900

    時刻文字列化のユーティリティ関数を作った

---
+++ atelier/time.c
@@ -0,0 +1,20 @@
+#include <time.h>
+#include <stdio.h>
+
+void PrintTime(time_t *tm, char* output, int length) {
+    struct tm *local;
+    local = localtime(tm);
+
+    snprintf(output, length, "%4d/%d/%d %d:%02d:%02d",
+              local->tm_year + 1900,
+              local->tm_mon + 1,
+              local->tm_mday,
+              local->tm_hour,
+              local->tm_min,
+              local->tm_sec);
+}
+
+void PrintCurrentTime(char* output, int length) {
+    time_t tm = time(NULL);
+    PrintTime(&tm, output, length);
+}
